bd5c57c40c2b9823724ea122fb318cb047a5a294
Kevin Mormandi <kevin1992@gmail.com>
2024-01-17 14:20:17 -0300

    update generate manifest script to match sample document structure

--- .github/generate-manifest.sh
+++ .github/generate-manifest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir output
-DOWNLOAD_URL_PREFIX="https://raw.githubusercontent.com/sensible-hq/sensible-configuration-library/main/" 
-LINES=$(find . -name "*.*" | grep -v "\/\." | grep ".json\|.pdf\|.png")
+DOWNLOAD_URL_PREFIX="https://raw.githubusercontent.com/sensible-hq/sensible-sample-documents/main/"
+LINES=$(find . -name "*.*" | grep -v "\/\." | grep ".json\|.pdf")
 CONFIGS=$(echo "$LINES" | grep "config.json")
 MANIFEST=$(
 for config in $CONFIGS; do
@@ -12,50 +12,29 @@ for config in $CONFIGS; do
     for json in $associated_jsons; do
         fileName=(${json//\.json/ })
         pdfFile="${fileName}_sample.pdf"
-        pngFile="${fileName}_sample.png"
-        if grep -q "$pdfFile" <<< "$LINES" && grep -q "$pngFile" <<< "$LINES";
+        if grep -q "$pdfFile" <<< "$LINES";
         then
             jsonPath=(${json//\.\// })
             pdfPath=(${pdfFile//\.\// })
-            pngPath=(${pngFile//\.\// })
             files+=(
-                "{\"path\":\"${jsonPath}\",\"download_url\":\"${DOWNLOAD_URL_PREFIX}${jsonPath}\"}" 
-                "{\"path\":\"${pdfPath}\",\"download_url\":\"${DOWNLOAD_URL_PREFIX}${pdfPath}\"}" 
-                "{\"path\":\"${pngPath}\",\"download_url\":\"${DOWNLOAD_URL_PREFIX}${pngPath}\"}"
-                )
+                "{\"path\":\"${jsonPath}\",\"download_url\":\"${DOWNLOAD_URL_PREFIX}${jsonPath}\"}"
+                "{\"path\":\"${pdfPath}\",\"download_url\":\"${DOWNLOAD_URL_PREFIX}${pdfPath}\"}"
+            )
         fi
     done
-
-    primary=$(cat $config | jq -s | jq '.[].featured.primary' | tr -d '"')
-    secondary=$(cat $config | jq -s | jq '.[].featured.secondary' | tr -d '"')
-
-    if test ${#primary} -gt 0; then
-        primary+='_sample.png'
-    else
-        primary="n/a"
-    fi
-
-    if test ${#secondary} -gt 0; then
-        secondary+='_sample.png'
-    else
-        secondary="n/a"
-    fi
 
     jsonFiles=$(echo "${files[@]}" | jq -r '{path:.path, download_url:.download_url}' | jq -s)
 
     echo $(
-        cat $config | jq -r --arg config_path ${config_path[1]} --argjson jsonFiles "${jsonFiles[@]}" --arg primary $primary --arg secondary $secondary '{
+        cat $config | jq -r --arg config_path ${config_path[1]} --argjson jsonFiles "${jsonFiles[@]}" '{
         config_data: {
             path: $config_path,
             category: .category,
             description: .description,
             display_name: .display_name,
-            featured: {
-                primary: $primary,
-                secondary: $secondary
-            }
+            icon: .icon,
         },
         files: $jsonFiles
     }')
-done | jq -s ) 
+done | jq -s )
 echo $MANIFEST >> output/manifest.json
