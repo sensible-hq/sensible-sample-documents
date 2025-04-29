c06d9f23b61c834b408d788a1729b9dd16d7ba57
Jay <78374858+sensiblejay@users.noreply.github.com>
2025-04-29 10:51:24 -0700

    add eu-west-3

--- .github/generator/src/generate-manifest.ts
+++ .github/generator/src/generate-manifest.ts
@@ -15,6 +15,7 @@ const DOWNLOAD_URL_PREFIX =
 const targets: Record<string, readonly string[]> = {
   "us-west-2": ["prod", "dev", "exp1"],
   "eu-west-2": ["prod"],
+  "eu-west-3": ["prod"],
   "ca-central-1": ["prod"],
 } as const;
 
